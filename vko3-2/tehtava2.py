24f8f6d857358961459230dde78aae9c0f8a7351
krkarizs <sevenke@gmail.com>
2021-07-09 10:36:23 +0300

    script for printing lines with command line attribute

--- vko3-2/tehtava2.py
+++ vko3-2/tehtava2.py
@@ -14,3 +14,20 @@ def download_blob(bucket_name, source_blob_name, destination_file_name):
     print("Blob {} downloaded to {}.".format(source_blob_name,destination_file_name))
 
 download_blob("checkpoint-storage-bucket", "checkpoint.txt", "C:/Users/KrisztinaKarizs/Desktop/Python exercises/checkpoint3/vko3-2/checkpoint.txt")
+
+#Printing the right number of lines from the command line
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("lines", help="displays the requested number of lines", type=int)
+
+args = parser.parse_args()
+
+with open("checkpoint.txt") as reader:
+    name_list = []
+
+    for line in reader:
+        name_list.append(line)
+
+for name in range(args.lines):
+    print(name_list[name])
